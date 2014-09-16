bda3446227bd637261d804966428e89b9421e772
Eiko Seino <eikoseino@hotmail.com>
2014-09-16 13:53:54 -0700

    adds database cleaner config

--- smiley_blog/spec/spec_helper.rb
+++ smiley_blog/spec/spec_helper.rb
@@ -39,9 +39,8 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
-end
 
-config.before(:suite) do
+  config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
   end
 
@@ -51,4 +50,8 @@ config.before(:suite) do
 
   config.after(:each) do
     DatabaseCleaner.clean
+  end
+
 end
+
+
