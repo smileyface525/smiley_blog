08b90f4217c539dcb7caa6628844211e375b7b7a
Eiko Seino <eikoseino@hotmail.com>
2014-09-19 14:44:58 -0700

    adds few more tests including #destroy and on session refactors

--- smiley_blog/spec/controllers/admin/sessions_controller_spec.rb
+++ smiley_blog/spec/controllers/admin/sessions_controller_spec.rb
@@ -2,33 +2,37 @@ require 'spec_helper'
 
 describe Admin::SessionsController do
 
-    describe "#create" do
+  describe "#create" do
 
-      context "if the user's email and password match" do
-        let!(:user) { FactoryGirl.create(:user) }
-        let!(:attributes) { { user: FactoryGirl.attributes_for(:user) } }
+    context "if the user's email and password match" do
+      let!(:user) { FactoryGirl.create(:user) }
+      let!(:attributes) { { user: FactoryGirl.attributes_for(:user) } }
 
-        it "starts session[:user_id]" do
-          post :create, attributes
-          expect(session[:user_id]).to eq user.id
-        end
-
-        it "redirects the user to /blogs/new" do
-          post :create, attributes
-          expect(response).to redirect_to new_blog_path
-        end
+      it "starts session[:user_id]" do
+        post :create, attributes
+        expect(session[:user_id]).to eq user.id
       end
 
-      context "if the user's email and password do not match" do
-
-        it "rerenders the login form"
+      it "redirects the user to /blogs/new" do
+        post :create, attributes
+        expect(response).to redirect_to root_path
       end
     end
 
-    describe "#destroy" do
+    context "if the user's email and password do not match" do
 
-      it "clears any ongoing sessions"
-
+      it "rerenders the login form"
     end
+  end
+
+  describe "#destroy" do
+
+    it "clears any ongoing sessions" do
+      user = FactoryGirl.create(:user)
+      delete :destroy, id: user.id
+      expect(session).to be_empty
+    end
+
+  end
 
 end
