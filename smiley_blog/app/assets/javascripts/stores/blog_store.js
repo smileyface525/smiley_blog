c99ff675f1c710b9ee123742a2c48d7d35d11931
Eiko Seino <eikoseino@hotmail.com>
2014-09-28 15:33:01 -0700

    implements BlogStore

--- smiley_blog/app/assets/javascripts/stores/blog_store.js
+++ smiley_blog/app/assets/javascripts/stores/blog_store.js
@@ -1,37 +1,71 @@
-var BlogStore = {
+var BlogStore = (function(){
 
-  getAllBlogs: function() {
-    $.ajax({
-      url: "/blogs",
-      success: function(allBlogs) {
-        this.blogList = allBlogs;
-        $(this).trigger('change');
-      }.bind(this)
-    })
-  },
+  var _blogs = [];
+  var CHANGE_EVENT = 'change';
+  var ActionTypes = BlogConstants.ActionTypes;
 
-  postBlog: function(event, blogData) {
-    $.ajax({
-      url: event.target.action,
-      type: "POST",
-      data: blogData
-    })
-    .done(function(newBlogs) {
-      this.blogList = newBlogs;
-      $(this).trigger('change');
-    }.bind(this))
-  },
+  return {
 
-  deleteBlog: function(blogId) {
-    var url = "/blogs/" + blogId
-    $.ajax({
-      url: url,
-      type: "DELETE"
-    })
-    .done(function(blogs) {
-      this.blogList = blogs;
-      $(this).trigger('change');
-    }.bind(this))
-  }
+    blogs: function() {
+      return _blogs;
+    },
+
+    getAllBlogs: function() {
+      $.ajax({
+        url: "/blogs",
+        success: function(allBlogs) {
+          this.blogList = allBlogs;
+          $(this).trigger('change');
+        }.bind(this)
+      })
+    },
+
+    postBlog: function(blogData) {
+      $.ajax({
+        url: '/blogs',
+        type: "POST",
+        data: blogData
+      })
+      .done(function(newBlogs) {
+        this.blogList = newBlogs;
+        this.triggerChange();
+      }.bind(this))
+    },
+
+    deleteBlog: function(blogId) {
+      var url = "/blogs/" + blogId
+      $.ajax({
+        url: url,
+        type: "DELETE"
+      })
+      .done(function(blogs) {
+        this.blogList = blogs;
+        this.triggerChange();
+      }.bind(this))
+    },
+
+    triggerChange: function() {
+      $(this).trigger(CHANGE_EVENT)
+    },
+
+    addChangeEvent: function(callback) {
+      $(this).on(CHANGE_EVENT, callback);
+    },
+
+    payload: function(payload) {
+      var action = payload.action;
+      switch(action.type) {
+        case ActionTypes.CREATE_BLOG:
+          this.postBlog(blogData);
+        break;
+        case ActionTypes.DELETE_BLOG:
+          this.deleteBlog(blogId);
+        break;
+      }
+    }
+
+  }()
+);
+
 
 }
