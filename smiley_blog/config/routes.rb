1d6ebc60be017dbdfe288a59c9c1b07562e7db4d
Eiko Seino <eikoseino@hotmail.com>
2014-09-30 14:05:50 -0700

    adds TagsController as well as route for it

--- smiley_blog/config/routes.rb
+++ smiley_blog/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :blogs
   namespace :admin do
-    resources :sessions, only: [:index, :create, :destroy]
+    resources :sessions, only: [:index, :new, :create, :destroy]
   end
+  resources :tags, only: [:index]
   root 'main#index'
 end
