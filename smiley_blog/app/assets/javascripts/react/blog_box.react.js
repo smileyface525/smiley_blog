b71b90560f2d46ca72c86a8a6567ca7795cca591
Eiko Seino <eikoseino@hotmail.com>
2014-09-30 13:57:22 -0700

    changes the name of the owner component 'BlogBox' to 'App'

--- smiley_blog/app/assets/javascripts/react/blog_box.react.js
+++ smiley_blog/app/assets/javascripts/react/blog_box.react.js
@@ -1,96 +1,44 @@
 /** @jsx React.DOM */
-//= require stores/user_store
-//= require react/blogs.react
-//= require react/blog_detail.react
-//= require react/login_form.react
-//= require stores/blog_store
+//= require stores/tag_store
 //= require react/navbar.react
-//= require react/blog_form.react
 
-var BlogBox = React.createClass({displayName: 'BlogBox',
+var App = React.createClass({displayName: 'App',
   getInitialState: function() {
-    UserStore.getCurrentUser();
-    return ( {
-      blogs: [],
-      showDetail: false,
-      loggedIn: UserStore.getLoginStatus(),
-      loginFailed: false,
-      showCreateForm: false,
-      blogToBeEdited: null
-    } )
+    return ({
+      tags: TagStore.tags()
+    })
   },
 
   componentDidMount: function() {
-    BlogStore.getAllBlogs();
-    this.bindListeners();
+    TagStore.addChangeEvent(function(){
+      this.setState({tags: TagStore.tags()})
+    }.bind(this))
+    TagStore.getAllTags();
   },
 
   render: function() {
-
-    if (this.state.showDetail === true && this.state.blogDetail !== undefined) {
-      return (
-        React.DOM.div(null, 
-          this.header(), 
-          BlogDetail({blogDetail: this.state.blogDetail, loggedIn: this.state.loggedIn}), 
-          React.DOM.a({href: "#", onClick: this.showList}, "Back to list")
-        )
-      )
-    }
-
-    else if (this.state.showCreateForm === true) {
-      return (
-        React.DOM.div(null, 
-          this.header(), 
-          BlogForm({blogToBeEdited: this.state.blogToBeEdited})
-        )
-      )
-    }
-
-    else {
-      return (
-        React.DOM.div(null, 
-          this.header(), 
-          Blogs({blogs: this.state.blogs, showDetail: this.state.showDetail})
-        )
-      )
-    }
-  },
-
-  bindListeners: function() {
-    $(BlogStore).on('change', function() {
-      this.setState({blogs: BlogStore.blogList, showDetail: false, showCreateForm: false, blogToBeEdited: null  });
-    }.bind(this))
-    $(Blog).on('showDetail', function(event, blogDetail) {
-      console.log('here');
-      this.setState({showDetail: true, blogDetail: blogDetail});
-    }.bind(this))
-    $(LoginForm).on('notFound', function() {
-      this.setState( {loginFailed: true});
-    }.bind(this))
-    $(UserStore).on('userLoggedIn', function() {
-      this.setState({loggedIn: true, loginFailed: false});
-    }.bind(this))
-    $(UserStore).on('userLoggedOut', function() {
-      document.location.reload(true);
-    }.bind(this))
-    $(Navbar).on('createBlog', function() {
-      this.setState({showCreateForm: true});
-    }.bind(this))
-    $(BlogDetail).on("editBlog", function(event, blog) {
-      this.setState({showDetail: false, showCreateForm: true, blogToBeEdited: blog})
-    }.bind(this))
-  },
-
-  showList: function(event) {
-    event.preventDefault();
-    this.setState({showDetail: false});
-  },
-
-  header: function() {
     return (
-      React.DOM.header({className: "absolute m0 center full-width top-0 bg-orange"}, 
-         this.state.loggedIn ? Navbar(null) : LoginForm({loginFailed: this.state.loginFailed}), 
-        React.DOM.h1({className: "bold"}, "sm:)eyblog")
+      React.DOM.div({className: "clearfix mr6 ml6"}, 
+        React.DOM.header({className: "center"}, 
+          React.DOM.h1(null, "sm", React.DOM.a({href: "#", className: "orange"}, ":)"), "eyblog"), 
+          React.DOM.p(null, "hello!"), 
+          Navbar({tags: this.state.tags})
+        ), 
+        React.DOM.section({className: "center mt3 mb3"}, 
+          React.DOM.h2({className: "regular orange"}, "All"), 
+          React.DOM.ul({className: "list-reset"}, 
+            React.DOM.li(null, "Blog1"), 
+            React.DOM.li(null, "Blog2"), 
+            React.DOM.li(null, "Blog3"), 
+            React.DOM.li(null, "Blog4"), 
+            React.DOM.li(null, "Blog5"), 
+            React.DOM.li(null, "Blog6")
+          )
+        ), 
+        React.DOM.footer({className: "center bor-dot-top light-gray"}, 
+          React.DOM.p({className: "m0 italic"}, "by eiko seino"), 
+          React.DOM.p(null, React.DOM.a({href: "", className: "light-gray italic"}, "login"))
+        )
       )
     )
   }
