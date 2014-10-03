22e4ce687599055e35ae98d41affbc4781307999
Eiko Seino <eikoseino@hotmail.com>
2014-10-02 21:19:29 -0700

    refactor: the app doesn't control which page to show now. The stores do.

--- smiley_blog/app/assets/javascripts/react/app.react.js
+++ smiley_blog/app/assets/javascripts/react/app.react.js
@@ -1,56 +1,83 @@
 /** @jsx React.DOM */
 //= require stores/tag_store
-//= require react/navbar.react
 //= require stores/blog_store
+//= require stores/session_store
+//= require react/header.react
+//= require react/navbar.react
+//= require react/login_form.react
 //= require react/blogs.react
 //= require react/blog_detail.react
+//= require react/blog_form.react
+//= require react/footer.react
 
 var App = React.createClass({displayName: 'App',
 
   getInitialState: function() {
     return ({
-      tags: TagStore.tags(),
+      tags: TagStore.allTags(),
       currentTag: TagStore.currentTag(),
       blogs: BlogStore.blogs(),
-      currentBlog: BlogStore.currentBlog()
+      currentBlog: BlogStore.currentBlog(),
+      currentPage: BlogStore.pageToShow(),
+      currentUesr: SessionStore.currentUser()
     })
   },
 
   componentDidMount: function() {
     TagStore.addTagsChangeEvent(function() {
-      this.setState({tags: TagStore.tags()});
+      this.setState({tags: TagStore.allTags()});
     }.bind(this));
     TagStore.addTagChangeEvent(function() {
       this.setState({currentTag: TagStore.currentTag()});
     }.bind(this));
     BlogStore.addChangeEvent(function() {
-      this.setState({blogs: BlogStore.blogs()});
+      this.setState({ blogs: BlogStore.blogs(), currentPage: BlogStore.pageToShow() });
     }.bind(this));
     BlogStore.addShowEvent(function() {
-      this.setState({currentBlog: BlogStore.currentBlog()});
+      this.setState({ currentBlog: BlogStore.currentBlog(), currentPage: BlogStore.pageToShow() });
     }.bind(this));
+    SessionStore.addShowEvent(function() {
+      this.setState({currentPage: SessionStore.pageToShow() });
+    }.bind(this)),
+    SessionStore.addChangeEvent(function() {
+      this.setState({currentPage: SessionStore.pageToShow(), currentUesr: SessionStore.currentUser()})
+    }.bind(this))
     TagStore.getAllTags();
     BlogStore.getBlogs(TagStore.currentTag());
+    SessionStore.getCurrentUser();
   },
 
   render: function() {
     return (
       React.DOM.div({className: "clearfix mr6 ml6"}, 
-        React.DOM.header({className: "center"}, 
-          React.DOM.h1(null, "sm", React.DOM.a({href: "#", className: "orange"}, ":)"), "eyblog"), 
-          React.DOM.p(null, "hello!"), 
-          Navbar({currentTag: this.state.currentTag, tags: this.state.tags})
-        ), 
-         this.state.currentBlog === null ?
-          Blogs({currentTag: this.state.currentTag, blogs: this.state.blogs}) :
-          BlogDetail({blog: this.state.currentBlog}), 
-        
-        React.DOM.footer({className: "center bor-dot-top light-gray"}, 
-          React.DOM.p({className: "m0 italic"}, "by eiko seino"), 
-          React.DOM.p(null, React.DOM.a({href: "", className: "light-gray italic"}, "login"))
-        )
+        Header({currentUser: this.state.currentUesr}), 
+        Navbar({currentTag: this.state.currentTag, tags: this.state.tags}), 
+         this.showCurrentPage(), 
+        Footer({currentUser: this.state.currentUesr, changeAppState: this.changeAppState})
       )
     )
+  },
+
+  showCurrentPage: function() {
+    var currentPage = this.state.currentPage;
+    switch (currentPage) {
+      case "blogDetail":
+        return BlogDetail({blog: this.state.currentBlog});
+      break;
+      case "loginForm":
+        return LoginForm(null)
+      break;
+      case "blogForm":
+      return BlogForm({blog: BlogStore.blogToEdit(), tags: TagStore.tags()});
+      break;
+      default:
+        return Blogs({currentTag: this.state.currentTag, blogs: this.state.blogs});
+      break;
+    }
+  },
+
+  changeAppState: function(state) {
+    this.setState(state);
   }
 
 })
