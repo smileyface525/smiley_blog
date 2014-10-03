0643757becb85043b4cfa2a1560b6a3f687798c5
Eiko Seino <eikoseino@hotmail.com>
2014-10-02 21:18:03 -0700

    adds login form

--- smiley_blog/app/assets/javascripts/react/login_form.react.js
+++ smiley_blog/app/assets/javascripts/react/login_form.react.js
@@ -1,40 +1,55 @@
 /** @jsx React.DOM */
-//= require stores/user_store
+//= require actions/session_actions
 
 var LoginForm = React.createClass({displayName: 'LoginForm',
+
+  getInitialState: function() {
+    return ({
+      errorMsg: null
+    });
+  },
+
+  inputNames: ['email', 'password'],
+
+  componentDidMount: function() {
+    SessionStore.addErrorEvent(function(event, errorMsg) {
+      this.setState({ errorMsg: errorMsg });
+    }.bind(this))
+  },
+
   render: function() {
     return (
-      React.DOM.div(null, 
-        React.DOM.p(null,  this.props.loginFailed ? "Please try again" : ' '), 
-        React.DOM.form({action: "admin/sessions", onSubmit: this.loginUser}, 
-          React.DOM.label(null, "email: "), 
-          React.DOM.input({ref: "email", type: "email", placeholder: "email", className: "input"}), 
-          React.DOM.label(null, "password: "), 
-          React.DOM.input({ref: "password", type: "password", placeholder: "password"}), 
-          React.DOM.input({type: "submit", value: "Login", className: "button-gray"})
+      React.DOM.section({className: "center mt3 mb3"}, 
+        React.DOM.h2({className: "regular orange"}, "Login"), 
+        React.DOM.p(null, this.state.errorMsg), 
+        React.DOM.form({action: "#", onSubmit: this.handleSubmit, className: ""}, 
+          React.DOM.input({ref: "email", type: "email", placeholder: "email", className: "block mx-auto half-width"}), 
+          React.DOM.input({ref: "password", type: "password", placeholder: "password", className: "block mx-auto half-width"}), 
+          React.DOM.input({type: "submit", value: "Login", className: "button-gray mx-auto"})
         )
       )
     )
   },
 
-  loginUser: function(event) {
+  handleSubmit: function(event) {
     event.preventDefault()
-    var email = this.refs.email.getDOMNode().value;
-    var password = this.refs.password.getDOMNode().value;
-    var loginData = {user: { email: email, password: password }}
-    this.submitLoginForm(event, loginData);
-    this.refs.email.getDOMNode().value = '';
-    this.refs.password.getDOMNode().value = '';
+    var loginData = this.getInputData(this.inputNames);
+    this.clearForm(this.inputNames);
+    SessionActions.login(loginData);
   },
 
-  submitLoginForm: function(event, loginData) {
-    $.ajax({
-      url: event.target.action,
-      type: "POST",
-      data: loginData
-    })
-    .done(function(userData){
-      userData.notFound ? $(LoginForm).trigger('notFound') : UserStore.setCurrentUser(userData)
-    })
+  getInputData: function(inputNames) {
+    var inputData = { user: {} };
+    inputNames.forEach(function(name) {
+      inputData.user[name] = this.refs[name].getDOMNode().value.trim();
+    }.bind(this));
+    return inputData;
+  },
+
+  clearForm: function(inputNames) {
+    inputNames.forEach(function(name) {
+      this.refs[name].getDOMNode().value = '';
+    }.bind(this));
   }
+
 })
--- smiley_blog/app/assets/react/login_form.react.js
+++ smiley_blog/app/assets/react/login_form.react.js
@@ -1,40 +1,55 @@
 /** @jsx React.DOM */
-//= require stores/user_store
+//= require actions/session_actions
 
 var LoginForm = React.createClass({
+
+  getInitialState: function() {
+    return ({
+      errorMsg: null
+    });
+  },
+
+  inputNames: ['email', 'password'],
+
+  componentDidMount: function() {
+    SessionStore.addErrorEvent(function(event, errorMsg) {
+      this.setState({ errorMsg: errorMsg });
+    }.bind(this))
+  },
+
   render: function() {
     return (
-      <div>
-        <p>{ this.props.loginFailed ? "Please try again" : ' ' }</p>
-        <form action="admin/sessions" onSubmit={this.loginUser} >
-          <label>email: </label>
-          <input ref="email" type="email" placeholder="email" className="input" />
-          <label>password: </label>
-          <input ref="password" type="password" placeholder="password"  />
-          <input type="submit" value="Login" className="button-gray"/>
+      <section className="center mt3 mb3">
+        <h2 className="regular orange">Login</h2>
+        <p>{this.state.errorMsg}</p>
+        <form action="#" onSubmit={this.handleSubmit} className="">
+          <input ref="email" type="email" placeholder="email" className="block mx-auto half-width" />
+          <input ref="password" type="password" placeholder="password" className="block mx-auto half-width" />
+          <input type="submit" value="Login" className="button-gray mx-auto"/>
         </form>
-      </div>
+      </section>
     )
   },
 
-  loginUser: function(event) {
+  handleSubmit: function(event) {
     event.preventDefault()
-    var email = this.refs.email.getDOMNode().value;
-    var password = this.refs.password.getDOMNode().value;
-    var loginData = {user: { email: email, password: password }}
-    this.submitLoginForm(event, loginData);
-    this.refs.email.getDOMNode().value = '';
-    this.refs.password.getDOMNode().value = '';
+    var loginData = this.getInputData(this.inputNames);
+    this.clearForm(this.inputNames);
+    SessionActions.login(loginData);
   },
 
-  submitLoginForm: function(event, loginData) {
-    $.ajax({
-      url: event.target.action,
-      type: "POST",
-      data: loginData
-    })
-    .done(function(userData){
-      userData.notFound ? $(LoginForm).trigger('notFound') : UserStore.setCurrentUser(userData)
-    })
+  getInputData: function(inputNames) {
+    var inputData = { user: {} };
+    inputNames.forEach(function(name) {
+      inputData.user[name] = this.refs[name].getDOMNode().value.trim();
+    }.bind(this));
+    return inputData;
+  },
+
+  clearForm: function(inputNames) {
+    inputNames.forEach(function(name) {
+      this.refs[name].getDOMNode().value = '';
+    }.bind(this));
   }
+
 })
