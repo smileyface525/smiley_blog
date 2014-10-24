a07184a6c15e4b6f37c8fad8a376387778a53fa8
Eiko Seino <eikoseino@hotmail.com>
2014-10-24 10:41:58 -0700

    adds minor modifications to the augular functionality

--- smiley_blog/app/assets/javascripts/main.js
+++ smiley_blog/app/assets/javascripts/main.js
@@ -1,2 +1,46 @@
-var app = angular.module('blog', []);
+(function(){
+
+  var app = angular.module('smileyblog', []);
+
+  app.controller('BlogController', function(){
+    this.blogs = myBlogs;
+    this.user = user;
+    this.currentBlog = 1;
+    this.setCurrentBlogTo = function(id) {
+      this.currentBlog = id;
+    };
+    this.currentBlogIs = function(id) {
+      return this.currentBlog === id;
+    };
+  });
+
+  var myBlogs = [
+    {
+      title: 'My Blog',
+      content: 'This is my first angular blog',
+      id: 1,
+      updatedOn: 2014-09-25,
+      confidential: false
+    },
+    {
+      title: 'Next Blog',
+      content: 'Second Blog',
+      id: 2,
+      updatedOn: 2014-05-25,
+      confidential: true
+    },
+    {
+      title: 'Third Blog',
+      content: 'This is the thrid Blog',
+      id: 3,
+      updatedOn: 2014-08-25,
+      confidential: false
+    }
+  ]
+
+  var user = {
+    loggedIn: true
+  }
+
+}());
 
